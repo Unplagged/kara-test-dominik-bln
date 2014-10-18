c40b5f705258c4a077a28eb48759e73704d9c71d
B Kleinen <kleinen@infrastructure.de>
2014-10-18 23:40:21 +0200

    moved variables further down

--- lab01_kara06/Kara06.java
+++ lab01_kara06/Kara06.java
@@ -8,12 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Kara06 extends Kara
 {
-    public static final int MAX_LENGTH = 20;
-    int length = 1;
     /**
      * Act - do whatever the Kara06 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
+    public static final int MAX_LENGTH = 20;
+    int length = 1;
+
     public void act() 
     {
         // Add your action code here.
